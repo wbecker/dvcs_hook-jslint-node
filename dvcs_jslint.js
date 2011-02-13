23f2da85d465b26ef0adb3acabed6f72ea9a16db
William Becker <wbekcer@gmail.com>
2011-02-13 10:12:04 +0000

    passes jslint

--- dvcs_jslint.js
+++ dvcs_jslint.js
@@ -65,7 +65,7 @@
   processFiles = function (files) {
     files.forEach(function (file) {
       exec(cmd + file, function (error, stdout, stderr) {
-        utils.log(cmd,file, stdout, stderr);
+        utils.log(cmd+" "+file+" "+ stdout+" "+ stderr);
         if (stdout.length > 0) {
           throw "JSLint error in " + file + ":\n" + stdout;
         }
